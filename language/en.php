c460cb3cc90a2543b0947f303559979b084160fa
George Dimitrakopoulos <gdimi@hyperworks.gr>
2014-12-09 00:25:32 +0200

    adding language files

---
+++ language/en.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * greek main language file
+ **/
+
+if (!defined('_w00t_frm')) die('har har har');
+
+$lang['controls-close'] = 'Close me';
+$lang['month-all'] = 'All'
+$lang['month-dec'] = 'Δεκέμβρης'
+$lang['month-nov'] = 'Νοέμβρης'
+$lang['month-oct'] = 'Οκτώβρης'
+$lang['month-sep'] = 'Σεπτέμβρης'
+$lang['month-aug'] = 'Αύγουστος'
+$lang['month-jul'] = 'Ιούλης'
+$lang['month-jun'] = 'Ιούνης'
+$lang['month-may'] = 'Μάης'
+$lang['month-apr'] = 'Απρίλης'
+$lang['month-mar'] = 'Μάρτης'
+$lang['month-feb'] = 'Φλεβάρης'
+$lang['month-jan'] = 'Ιανουάριος'
+$lang['month'] = 'Μήνας'
+$lang['year'] = 'Έτος'
+$lang['lmenu'] = 'Menu';
+$lang['lmenu-add-case'] = 'add Case';
+$lang['lmenu-add-client'] = 'add Client';
+$lang['lmenu-all-clients'] = 'all Clients';
+$lang['lmenu-income'] = 'έσοδα';
+$lang['lmenu-expenses'] = 'έξοδα';
+$lang['lmenu-events'] = 'events';
+$lang['lmenu-cms'] = 'CMS';
+$lang['lmenu-stats'] = 'stats';
+$lang['lmenu-settings'] = 'settings';
+$lang['lmenu-various'] = 'Διάφορα';
+$lang['lmenu-prices'] = 'κοστολόγιο';
+$lang['lmenu-todo'] = 'this->todo';
+$lang['case-filter-filter'] = 'Filter:';
+$lang['case-filter-closed'] = 'Closed';
+$lang['case-filter-open'] = 'Open';
+$lang['case-filter-high'] = 'High';
+$lang['case-filter-user'] = 'User';
+$lang['client-card'] = 'Καρτέλα Πελάτη';
+$lang['client-delete-btn'] = 'Delete Client';
+$lang['client-delete-title'] = 'DELETE CLIENT!';
+$lang['client-delete-msg'] = 'Sigoura i tha kaneis xazomara? Delete';
+$lang['client-delete-yes'] = 'YES';
+$lang['client-delete-no'] = 'no';
+$lang['cms'] = 'CMS';
+$lang['cms-msg'] = '(δλδ Content Management System)';
+$lang['cms-intro'] = 'Εδώ μπορείτε να κάνετε edit το message of the day (motd) και τον πίνακα που γράφουμε γενικώς διάφορα (board)
+';
+$lang['cms-motd'] = 'Message of the day';
+$lang['cms-motd-save'] = 'Save motd';
+$lang['cms-board'] = 'Board';
+$lang['cms-board-save'] = 'Save board';
+$lang['cms-save-all'] = 'Save all';
+$lang['cms-save-ok'] = 'Ok all updated!';
+$lang['costs'] = 'Έξοδα';
+$lang['costs-add'] = 'προσθήκη εξόδου';
+$lang['costs-new'] = 'Νέο έξοδο';
+$lang['costs-descr'] = 'Περιγραφή';
+$lang['costs-amount'] = 'Ποσό';
+$lang['costs-date'] = 'Ημ/νια';
+$lang['costs-add-msg'] = 'Προσθήκη λυπητερής';
+$lang['costs-edit'] = 'Επεξεργασία εξόδου';
+$lang['costs-save'] = 'Αποθήκευση λυπητερής';
+$lang['costs-delete'] = 'ΔΙΑΓΡΑΦΗ ΛΥΠΗΤΕΡΗΣ!';
+$lang['costs-delete-msg'] = 'Sigoura i tha kaneis xazomara? Delete';
+$lang['costs-delete-yes'] = 'YES';
+$lang['costs-delete-no'] = 'NO';
+$lang['costs-error1'] = 'Re vale perigrafh, poso kai imerominia!';
+$lang['costs-error2'] = 'zero id is not possible';
+
+
+?> 
+ 

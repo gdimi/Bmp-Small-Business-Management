f251faae10248cb4ce417a4b415534076b233615
George Dimitrakopoulos <gdimi@hyperworks.gr>
2015-01-02 13:26:15 +0200

    language abstraction and added 2 missing months for select

--- tpl/html/income.inc.php
+++ tpl/html/income.inc.php
@@ -1,22 +1,25 @@
-		<span class="cl-b" onclick="$(this).parent().toggle();">Close me</span>
-		<h2>Έσοδα</h2>
+		<span class="cl-b" onclick="$(this).parent().toggle();"><?php echo $lang['controls-close']; ?></span>
+		<h2><?php echo $lang['costs']; ?></h2>
         <span class="filters" id="income_filters">
             <form>
-                year <select id="income_year">
+                <?php echo $lang['year']; ?> <select id="income_year">
+                    <option value="2015" selected>2015</option>
                     <option value="2014" selected>2014</option>
                 </select> 
-                month <select id="income_month">
-                    <option value="0" selected>All</option>
-                    <option value="12">12 - Δεκέμβρης</option>                     
-                    <option value="11">11 - Νοέμβρης</option>                    
-                    <option value="10">10 - Οκτώβρης</option>                    
-                    <option value="9">9 - Σεπτέμβρης</option>                    
-                    <option value="8">8 - Αύγουστος</option>
-                    <option value="7">7 - Ιούλης</option>
-                    <option value="6">6 - Ιούνης</option>
-                    <option value="5">5 - Μάης</option>
-                    <option value="4">4 - Απρίλης</option>
-                    <option value="3">3 - Μάρτης</option>
+                <?php echo $lang['month']; ?> <select id="income_month">
+                    <option value="0" selected><?php echo $lang['month-all']; ?></option>
+                    <option value="12">12 - <?php echo $lang['month-dec']; ?></option>                     
+                    <option value="11">11 - <?php echo $lang['month-nov']; ?></option>                    
+                    <option value="10">10 - <?php echo $lang['month-oct']; ?></option>                    
+                    <option value="9">9 - <?php echo $lang['month-sep']; ?></option>                    
+                    <option value="8">8 - <?php echo $lang['month-aug']; ?></option>
+                    <option value="7">7 - <?php echo $lang['month-jul']; ?></option>
+                    <option value="6">6 - <?php echo $lang['month-jun']; ?></option>
+                    <option value="5">5 - <?php echo $lang['month-may']; ?></option>
+                    <option value="4">4 - <?php echo $lang['month-apr']; ?></option>
+                    <option value="3">3 - <?php echo $lang['month-mar']; ?></option>
+                    <option value="2">2 - <?php echo $lang['month-feb']; ?></option>
+                    <option value="1">1 - <?php echo $lang['month-jan']; ?></option>
                 </select>
             </form>
         </span>
@@ -41,19 +44,4 @@ $('#esoda').on('change','select',function(){
 		});
 	}
 });
-/*$("#income").click(function() {
-		$.get("index.php",
-		{task: "esoda",pos: "before"},
-		function(data, textStatus, jqXHR){
-			if(data.status === "success") {
-				$("#esoda div").html(data.message);
-				$("#esoda").show("fast");
-			} else if(data.status === "error") {
-				$("#esoda div").append(data.message);
-				$("#esoda").show("fast").delay(2000).hide("slow");
-			}
-		}, "json").fail(function(jqXHR, textStatus, errorThrown){
-			$("#esoda").show("fast").append(textStatus);
-		});
-});*/
 </script>

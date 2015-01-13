856e5fa04ba49a0c833f432d5fcb85c3eb4d1f7d
George Dimitrakopoulos <gdimi@hyperworks.gr>
2015-01-13 11:52:25 +0200

    fix for issue #22

--- sources/tasks/cmsUpdate.task.php
+++ sources/tasks/cmsUpdate.task.php
@@ -1,22 +1,24 @@
 <?php
-//read cms content
+//update cms content
 if (!defined('_w00t_frm')) die('har har har');
- 
+
+$scerr = ''; //var to hold error msg. Initialized here so functions below can access it by addr
+
 if (!$pos or $pos != 'before') {
 	$scerr = 'Task ['.$task.'] warning: no or wrong position of execution';
 } else {
     if (isset($_POST['what'])) { $what = $_POST['what']; }
-    if (isset($_POST['motd'])) { $motd = substr($_POST['motd'],0,128); }
+    if (isset($_POST['motd'])) { $motd = substr($_POST['motd'],0,128); } //motd will be 128 chars long
     if (isset($_POST['board'])) { $board = $_POST['board']; }
     if ($what == 'all') {
         $res = UpdateAll($motd,$board,$scerr);
-        if (!$res) { $scerr = "updated all failed!"; }
+        if ($res === false) { $scerr .= "updated all failed!"; } //because motd * board may return 0, we have to use === operator
     } elseif ($what == 'motd') {
         $res = UpdateMotd($motd,$scerr);
-        if (!$res) { $scerr = "updated motd failed!"; }
+        if ($res === false) { $scerr .= "updated motd failed!"; }
     } elseif ($what == 'board') {
         $res = UpdateBoard($board,$scerr);
-        if (!$res) { $scerr = "updated board failed!"; }
+        if ($res === false) { $scerr .= "updated board failed!"; }
     } else {
         $scerr = 'Uknown directive: '.$what;
     }
@@ -44,6 +46,7 @@ function UpdateMotd($motd,&$scerr) {
         return $motd = file_put_contents("content/motd",$motd);
     } else {
 		$scerr .= "motd file not found!";
+		return false;
 	}
 }
 
@@ -52,6 +55,7 @@ function UpdateBoard($board,&$scerr) {
         return $board = file_put_contents("content/board",$board);
     } else {
 		$scerr .= "board file not found!";
+		return false;
 	}
 }
 

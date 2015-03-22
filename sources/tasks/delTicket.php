551e0b337f7a86049151f9b32ffd0cce0412d28c
George Dimitrakopoulos <gdimi@hyperworks.gr>
2015-03-22 20:57:19 +0200

    fixed licence to gpl2

--- sources/tasks/delTicket.php
+++ sources/tasks/delTicket.php
@@ -6,11 +6,10 @@
  * 
  * PHP version 5
  *
- * LICENSE: This source file is subject to version 3.01 of the PHP license
- * that is available through the world-wide-web at the following URI:
- * http://www.php.net/license/3_01.txt.  If you did not receive a copy of
- * the PHP License and are unable to obtain it through the web, please
- * send a note to license@php.net so we can mail you a copy immediately.
+ * LICENCE: This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version
  *
  * @category   bmp\sources\ajax handlers
  * @package    bmp\sources

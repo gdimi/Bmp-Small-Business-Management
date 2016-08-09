65b881e1ee6ef14c36028d19d2685a44e4288ed4
gdimi <gdimi@hyperworks.gr>
2016-08-09 15:09:04 +0300

    added trash object view task file

---
+++ sources/tasks/trashObj.task.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * view object trash task
+ * views details of an object
+ */
+if (!defined('_w00t_frm')) die('har har har');
+
+if (!$pos or $pos != 'before') {
+	$scerr = 'Task ['.$task.'] warning: no or wrong position of execution';
+} else {
+    include_once('sources/class.trash.php');
+    if (is_dir("content/trashed")) {
+        $Trash = new Trash;
+        $Trash->initTrash();
+        if (!$Trash->trashErr) {
+
+            $tk_status = json_encode(array(
+             'status'=>'success',
+             'data' => $data
+            ));
+            echo $tk_status;
+            exit(0);
+        } else {
+            $scerr = $Trash->trashErr;
+        }
+    } else {
+        $scerr = 'Trash folder not found';
+    }
+
+}
+
+if ($scerr) {
+	$tk_status = json_encode(array(
+	 'status' => 'error',
+	 'message'=> $scerr.'<br />'
+	));
+	echo $tk_status;
+	exit(1);
+}
+?>

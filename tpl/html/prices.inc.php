98f2a7e175d592b68678a311b77ea480f8e2472e
gdimi <gdimi@hyperworks.gr>
2016-12-29 15:53:21 +0200

    issue #72

--- tpl/html/prices.inc.php
+++ tpl/html/prices.inc.php
@@ -1,8 +1,4 @@
 <h2>Price table</h2>
 <hr size="1" />
-<pre>
-	everything in here is between pre tags so to preserve text format
-1) Price group one
-	service1: 10e
-	service2: 20e
-</pre>
+<?php include('content/prices.html'); ?>
+

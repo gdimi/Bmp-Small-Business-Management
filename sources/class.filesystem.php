15d22aa5878dbe45393a106ceb8ac46df2ee4909
George Dimitrakopoulos <gdimi@hyperworks.gr>
2017-12-26 01:18:05 +0200

    for issue #5 add upload attachment removal

---
+++ sources/class.filesystem.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * filesystem class
+ *
+ * this file contains the main filesystem class
+ * 
+ * PHP version 5+
+ *
+ * LICENCE: This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version
+ *
+ * @category   bmp\sources\classes
+ * @package    bmp\sources
+ * @author     Original Author <gdimi@hyperworks.gr>
+ * @copyright  2014-2017 George Dimitrakopoulos
+ * @license    GPLv2
+ * @version    1.0
+ * @link       -
+ * @see        -
+ * @since      Since 0.589-dev
+ * @deprecated -
+ */
+if (!defined('_w00t_frm')) die('har har har');
+
+class Trash {
+
+    public $fsErr;
+    
+	function __construct() {
+		$this->trashErr = '';
+	}
+	
+}
+
+?>

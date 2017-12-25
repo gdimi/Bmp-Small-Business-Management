48859cb4d508acb8f84c9179e517831ab0b5e4a1
George Dimitrakopoulos <gdimi@hyperworks.gr>
2017-12-26 00:15:22 +0200

    work on #58 trash: view object details

--- sources/tasks/trashObj.task.php
+++ sources/tasks/trashObj.task.php
@@ -8,25 +8,31 @@ if (!defined('_w00t_frm')) die('har har har');
 if (!$pos or $pos != 'before') {
 	$scerr = 'Task ['.$task.'] warning: no or wrong position of execution';
 } else {
-    include_once('sources/class.trash.php');
-    if (is_dir("content/trashed")) {
-        $Trash = new Trash;
-        $Trash->initTrash();
-        if (!$Trash->trashErr) {
-
-            $tk_status = json_encode(array(
-             'status'=>'success',
-             'data' => $data
-            ));
-            echo $tk_status;
-            exit(0);
-        } else {
-            $scerr = $Trash->trashErr;
-        }
-    } else {
-        $scerr = 'Trash folder not found';
-    }
-
+	$to = $_GET['to'];
+	if ($to) {	
+		include_once('sources/class.trash.php');
+		if (is_dir("content/trashed")) {
+			$Trash = new Trash;
+			$Trash->initTrash();
+			if (!$Trash->trashErr) {
+				$to_data = $Trash->showObjectDetails('content/trashed/'.$to);
+				if (!$Trash->trashErr) {
+					$tk_status = json_encode(array(
+					 'status'=>'success',
+					 'data' => $to_data
+					));
+					echo $tk_status;
+					exit(0);
+				} else {
+					$scerr = $Trash->trashErr;
+				}
+			} else {
+				$scerr = $Trash->trashErr;
+			}
+		} else {
+			$scerr = 'Trash folder not found';
+		}
+	}
 }
 
 if ($scerr) {

ab932d1d22dc0a90f503ec0fae187aec32fa4f8e
George Dimitrakopoulos <gdimi@hyperworks.gr>
2020-09-19 11:05:54 +0300

    deny direct access

--- sources/class.tickets.php
+++ sources/class.tickets.php
@@ -23,6 +23,8 @@
  * @deprecated -
  */
 
+if (!defined('_w00t_frm')) die('har har har');
+
 class tickets extends db {
 	public $historyfile = 'content/action_history.txt';
 	public $tickets = Array();

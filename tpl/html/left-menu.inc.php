391b259dc698eb2fa5686050266d72157eeeee71
George Dimitrakopoulos <gdimi@hyperworks.gr>
2021-04-04 13:06:36 +0300

    fix html and style in left menu

--- tpl/html/left-menu.inc.php
+++ tpl/html/left-menu.inc.php
@@ -2,51 +2,50 @@
         <ul>
             <li>
 				<span class="mcap">
-					<a href="javascript:void(0);" onclick="$('#new_ticket').toggle();" style="color:white;text-decoration:none;"><?php echo $lang['lmenu-add-case']; ?></a>
-				</span>
-				<a href="javascript:$(this).preventDefault();" id="add_tk" title="add a new case" alt="add a new case"><img src="images/add.png" width="24" /></a>
+					<a href="javascript:void(0);" onclick="$('#new_ticket').toggle();"><?php echo $lang['lmenu-add-case']; ?></a>
+				
+				<span" id="add_tk" title="add a new case" alt="add a new case"><img src="images/add.png" width="24" /></span></span>
 			</li>
             <li>
 				<span class="mcap">
-					<a href="javascript:void(0);" onclick="$('#new_client').toggle();" style="color:white;text-decoration:none;"><?php echo $lang['lmenu-add-client']; ?></a>
-				</span>
-				<span id="add_client"></span>
+					<a href="javascript:void(0);" onclick="$('#new_client').toggle();"><?php echo $lang['lmenu-add-client']; ?></a>
+				
+				<span id="add_client"></span></span>
 			</li>
             <li>
-				<span class="mcap"><a href="javascript:void(0);" onclick="$('#allclients').toggle();" style="color:white;text-decoration:none;"><?php echo $lang['lmenu-all-clients']; ?></a></span>
-				<span id="all_clients"></span>
+				<span class="mcap"><a href="javascript:void(0);" onclick="$('#allclients').toggle();"><?php echo $lang['lmenu-all-clients']; ?></a>
+				<span id="all_clients"></span></span>
 			</li>
             <li>
-				<span class="mcap"><a href="javascript:void(0);" onclick="$('#esoda').toggle();" style="color:white;text-decoration:none;"><?php echo $lang['lmenu-income']; ?></a></span>
-				<span id="income"></span>
+				<span class="mcap"><a href="javascript:void(0);" onclick="$('#esoda').toggle();"><?php echo $lang['lmenu-income']; ?></a>
+				<span id="income"></span></span>
 			</li>
             <li>
-				<span class="mcap"><a href="javascript:void(0);" onclick="$('#costs').toggle();" style="color:white;text-decoration:none;"><?php echo $lang['lmenu-expenses']; ?></a></span>
-				<span id="expenses"></span>
+				<span class="mcap"><a href="javascript:void(0);" onclick="$('#costs').toggle();"><?php echo $lang['lmenu-expenses']; ?></a>
+				<span id="expenses"></span></span>
 			</li>
             <li style="display:none;">
-				<span class="mcap"><a href="javascript:void(0);" onclick="$('#events').toggle();" style="color:white;text-decoration:none;"><?php echo $lang['lmenu-events']; ?></a></span>
-				<span id="events"></span>
+				<span class="mcap"><a href="javascript:void(0);" onclick="$('#events').toggle();"><?php echo $lang['lmenu-events']; ?></a></span>
 			</li>
             <li>
-				<span class="mcap"><a href="javascript:void(0);" onclick="$('#cms_info').toggle();" style="color:white;text-decoration:none;"><?php echo $lang['lmenu-cms']; ?></a></span>
-				<span id="cms"></span>
+				<span class="mcap"><a href="javascript:void(0);" onclick="$('#cms_info').toggle();"><?php echo $lang['lmenu-cms']; ?></a>
+				<span id="cms"></span></span>
 			</li>
             <li>
-				<span class="mcap"><a href="javascript:void(0);" onclick="$('#stats_info').toggle();" style="color:white;text-decoration:none;"><?php echo $lang['lmenu-stats']; ?></a></span> 
-				<span id="stats"></span>
+				<span class="mcap"><a href="javascript:void(0);" onclick="$('#stats_info').toggle();"><?php echo $lang['lmenu-stats']; ?></a> 
+				<span id="stats"></span></span>
 			</li>
             <li>
-				<span class="mcap"><a href="javascript:void(0);" onclick="$('#settings').toggle();" style="color:white;text-decoration:none;"><?php echo $lang['lmenu-settings']; ?></a></span>
-				<span id="settings_tk"></span>
+				<span class="mcap"><a href="javascript:void(0);" onclick="$('#settings').toggle();"><?php echo $lang['lmenu-settings']; ?></a>
+				<span id="settings_tk"></span></span>
 			</li>
             <li>
-				<span class="mcap"><a href="javascript:void(0);" onclick="$('#various').toggle();" style="color:white;text-decoration:none;"><?php echo $lang['lmenu-various']; ?></a></span>
+				<span class="mcap"><a href="javascript:void(0);" onclick="$('#various').toggle();"><?php echo $lang['lmenu-various']; ?></a></span>
 			</li>
             <li>
-				<span class="mcap"><a href="javascript:void(0);" onclick="$('#prices').toggle();" style="color:white;text-decoration:none;"><?php echo $lang['lmenu-prices']; ?></a></span>
+				<span class="mcap"><a href="javascript:void(0);" onclick="$('#prices').toggle();"><?php echo $lang['lmenu-prices']; ?></a></span>
 			</li>
             <li style="display:none;">
-				<span class="mcap"><a href="javascript:void(0);" onclick="$('#this_todo').toggle();" style="color:white;text-decoration:none;"><?php echo $lang['lmenu-todo']; ?></a></span>
+				<span class="mcap"><a href="javascript:void(0);" onclick="$('#this_todo').toggle();"><?php echo $lang['lmenu-todo']; ?></a></span>
 			</li>
         </ul>

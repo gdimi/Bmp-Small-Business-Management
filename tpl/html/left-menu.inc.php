6f2802f0f77881f3df5c95ff0b9467fba859a870
George Dimitrakopoulos <gdimi@hyperworks.gr>
2021-04-04 13:10:08 +0300

    properly trigger the right element click and not just toggle divs

--- tpl/html/left-menu.inc.php
+++ tpl/html/left-menu.inc.php
@@ -2,41 +2,41 @@
         <ul>
             <li>
 				<span class="mcap">
-					<a href="javascript:void(0);" onclick="$('#new_ticket').toggle();"><?php echo $lang['lmenu-add-case']; ?></a>
+					<a href="javascript:void(0);" onclick="$('#add_tk').click();"><?php echo $lang['lmenu-add-case']; ?></a>
 				
 				<span" id="add_tk" title="add a new case" alt="add a new case"><img src="images/add.png" width="24" /></span></span>
 			</li>
             <li>
 				<span class="mcap">
-					<a href="javascript:void(0);" onclick="$('#new_client').toggle();"><?php echo $lang['lmenu-add-client']; ?></a>
+					<a href="javascript:void(0);" onclick="$('#add_client').click();"><?php echo $lang['lmenu-add-client']; ?></a>
 				
 				<span id="add_client"></span></span>
 			</li>
             <li>
-				<span class="mcap"><a href="javascript:void(0);" onclick="$('#allclients').toggle();"><?php echo $lang['lmenu-all-clients']; ?></a>
+				<span class="mcap"><a href="javascript:void(0);" onclick="$('#all_clients').click();"><?php echo $lang['lmenu-all-clients']; ?></a>
 				<span id="all_clients"></span></span>
 			</li>
             <li>
-				<span class="mcap"><a href="javascript:void(0);" onclick="$('#esoda').toggle();"><?php echo $lang['lmenu-income']; ?></a>
+				<span class="mcap"><a href="javascript:void(0);" onclick="$('#income').click();"><?php echo $lang['lmenu-income']; ?></a>
 				<span id="income"></span></span>
 			</li>
             <li>
-				<span class="mcap"><a href="javascript:void(0);" onclick="$('#costs').toggle();"><?php echo $lang['lmenu-expenses']; ?></a>
+				<span class="mcap"><a href="javascript:void(0);" onclick="$('#expenses').click();"><?php echo $lang['lmenu-expenses']; ?></a>
 				<span id="expenses"></span></span>
 			</li>
             <li style="display:none;">
 				<span class="mcap"><a href="javascript:void(0);" onclick="$('#events').toggle();"><?php echo $lang['lmenu-events']; ?></a></span>
 			</li>
             <li>
-				<span class="mcap"><a href="javascript:void(0);" onclick="$('#cms_info').toggle();"><?php echo $lang['lmenu-cms']; ?></a>
+				<span class="mcap"><a href="javascript:void(0);" onclick="$('#cms').click();"><?php echo $lang['lmenu-cms']; ?></a>
 				<span id="cms"></span></span>
 			</li>
             <li>
-				<span class="mcap"><a href="javascript:void(0);" onclick="$('#stats_info').toggle();"><?php echo $lang['lmenu-stats']; ?></a> 
+				<span class="mcap"><a href="javascript:void(0);" onclick="$('#stats').click();"><?php echo $lang['lmenu-stats']; ?></a> 
 				<span id="stats"></span></span>
 			</li>
             <li>
-				<span class="mcap"><a href="javascript:void(0);" onclick="$('#settings').toggle();"><?php echo $lang['lmenu-settings']; ?></a>
+				<span class="mcap"><a href="javascript:void(0);" onclick="$('#settings_tk').click();"><?php echo $lang['lmenu-settings']; ?></a>
 				<span id="settings_tk"></span></span>
 			</li>
             <li>

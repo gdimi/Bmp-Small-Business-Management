807f7e4a3b9601656a454ca463ca081fc5d3f288
George Dimitrakopoulos <gdimi@hyperworks.gr>
2023-05-19 21:35:31 +0300

    proper lang vars

--- tpl/html/left-menu.inc.php
+++ tpl/html/left-menu.inc.php
@@ -9,41 +9,40 @@
             <li>
 				<span class="mcap">
 					<a href="javascript:void(0);" onclick="$('#add_client').click();"><?php echo $lang['lmenu-add-client']; ?></a>
-				
-				<span id="add_client"></span></span>
+				<span id="add_client" title="<?php echo $lang['lmenu-add-client']; ?>"></span></span>
 			</li>
             <li>
 				<span class="mcap"><a href="javascript:void(0);" onclick="$('#all_clients').click();"><?php echo $lang['lmenu-all-clients']; ?></a>
-				<span id="all_clients"></span></span>
+				<span id="all_clients" title="<?php echo $lang['lmenu-all-clients']; ?>"></span></span>
 			</li>
             <li>
 				<span class="mcap"><a href="javascript:void(0);" onclick="$('#income').click();"><?php echo $lang['lmenu-income']; ?></a>
-				<span id="income"></span></span>
+				<span id="income" title="<?php echo $lang['lmenu-income']; ?>"></span></span>
 			</li>
             <li>
 				<span class="mcap"><a href="javascript:void(0);" onclick="$('#expenses').click();"><?php echo $lang['lmenu-expenses']; ?></a>
-				<span id="expenses"></span></span>
+				<span id="expenses" title="<?php echo $lang['lmenu-expenses']; ?>"></span></span>
 			</li>
             <li style="display:none;">
-				<span class="mcap"><a href="javascript:void(0);" onclick="$('#events').toggle();"><?php echo $lang['lmenu-events']; ?></a></span>
+				<span class="mcap"><a href="javascript:void(0);" onclick="$('#events').toggle();" title="<?php echo $lang['lmenu-events']; ?>"><?php echo $lang['lmenu-events']; ?></a></span>
 			</li>
             <li>
 				<span class="mcap"><a href="javascript:void(0);" onclick="$('#cms').click();"><?php echo $lang['lmenu-cms']; ?></a>
-				<span id="cms"></span></span>
+				<span id="cms" title="<?php echo $lang['lmenu-cms']; ?>"></span></span>
 			</li>
             <li>
 				<span class="mcap"><a href="javascript:void(0);" onclick="$('#stats').click();"><?php echo $lang['lmenu-stats']; ?></a> 
-				<span id="stats"></span></span>
+				<span id="stats" title="<?php echo $lang['lmenu-stats']; ?>"></span></span>
 			</li>
             <li>
 				<span class="mcap"><a href="javascript:void(0);" onclick="$('#settings_tk').click();"><?php echo $lang['lmenu-settings']; ?></a>
-				<span id="settings_tk"></span></span>
+				<span id="settings_tk" title="<?php echo $lang['lmenu-settings']; ?>"></span></span>
 			</li>
             <li>
-				<span class="mcap"><a href="javascript:void(0);" onclick="$('#various').toggle();"><?php echo $lang['lmenu-various']; ?></a></span>
+				<span class="mcap"><a href="javascript:void(0);" onclick="$('#various').toggle();"  title="<?php echo $lang['lmenu-various']; ?>"><?php echo $lang['lmenu-various']; ?></a></span>
 			</li>
             <li>
-				<span class="mcap"><a href="javascript:void(0);" onclick="$('#prices').toggle();"><?php echo $lang['lmenu-prices']; ?></a></span>
+				<span class="mcap"><a href="javascript:void(0);" onclick="$('#prices').toggle();"  title="<?php echo $lang['lmenu-prices']; ?>"><?php echo $lang['lmenu-prices']; ?></a></span>
 			</li>
             <li style="display:none;">
 				<span class="mcap"><a href="javascript:void(0);" onclick="$('#this_todo').toggle();"><?php echo $lang['lmenu-todo']; ?></a></span>
